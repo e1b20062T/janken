2a9e41fb54a11403e5641897e8d4f8ad5d13957c
e1b20062T <e1b20062@oit.ac.jp>
2022-10-16 21:36:05 +0900

    htmlの表示

--- src/main/java/oit/is/z0809/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z0809/kaizi/janken/controller/JankenController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import oit.is.z0809.kaizi.janken.model.janken;
+//import oit.is.z0809.kaizi.janken.model.janken;
 
 @Controller
 public class JankenController {
@@ -23,13 +23,13 @@ public class JankenController {
     return "janken.html";
   }
 
-  @GetMapping("/jankengame")
-  public String jankengame(@RequestParam String hand, ModelMap model) {
-    String yourhand = hand;
-    String cpuhand = "Gu";
-    Janken janken = new Janken(yourhand, cpuhand);
-    model.addAttribute("yourhand", yourhand);
+//  @GetMapping("/jankengame")
+//  public String jankengame(@RequestParam String hand, ModelMap model) {
+//    String yourhand = hand;
+//    String cpuhand = "Gu";
+//    Janken janken = new Janken(yourhand, cpuhand);
+//    model.addAttribute("yourhand", yourhand);
 
+//  }
 
-  }
 }
--- src/main/java/oit/is/z0809/kaizi/janken/model/janken.java
+++ src/main/java/oit/is/z0809/kaizi/janken/model/janken.java
@@ -1,12 +1,16 @@
 package oit.is.z0809.kaizi.janken.model;
 
-public class Janken {
-  String hand;
-  String cpuhand;
-  String result;
+//public class Janken {
+//  String hand;
+//  String cpuhand;
+//  String result;
 
-  public Janken(String hand, String cpuhand) {
+//public Janken(String hand, String cpuhand) {
 
-  }
+//if (hand == "Gu"){
 
-}
+//}
+
+//}
+
+//}

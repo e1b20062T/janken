b1d612c7456aba16f1943151b17cefdbc26434d8
e1b20062T <e1b20062@oit.ac.jp>
2022-10-17 16:00:36 +0900

    ログイン、ログアウト処理実装

--- src/main/java/oit/is/z0809/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z0809/kaizi/janken/controller/JankenController.java
@@ -2,36 +2,20 @@ package oit.is.z0809.kaizi.janken.controller;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//import oit.is.z0809.kaizi.janken.model.janken;
-
 @Controller
 @RequestMapping("/janken")
 public class JankenController {
-
-  @GetMapping("/janken")
+  @GetMapping("step1")
   public String janken() {
     return "janken.html";
-  }
-
-  /**
-   *
-   * @param model Thymeleafにわたすデータを保持するオブジェクト
-   * @param prin  ログインユーザ情報が保持されるオブジェクト
-   * @return
-   */
-  @GetMapping("step2")
-  public String sample32(ModelMap model, Principal prin) {
-    String loginUser = prin.getName(); // ログインユーザ情報
-    model.addAttribute("login_user", loginUser);
-    return "sample31.html";
   }
 
   // @GetMapping("/janken")
